4dd721ec7cf178dc9b317a89b477a197e84dc234
stepheno <stepheno@student.chalmers.se>
2023-07-12 18:02:00 +0200

    First Commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ticketIt.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
